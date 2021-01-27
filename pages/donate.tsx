7d0676a1a2b1a06f585d75656d32b6c147ea5a2a
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-01-27 16:47:57 -0500

    Create coming soon message

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,7 +1,16 @@
 import { HeroLayout } from "@/components/Layout"
+import { Grid, Typography } from "@material-ui/core"
 
 const DonatePage: React.FC = () => {
-	return <HeroLayout title="Donate"></HeroLayout>
+	return (
+		<HeroLayout title="Donate">
+			<Grid item xs={12}>
+				<Typography variant="h3" color="textSecondary" align="center">
+					Coming soon!
+				</Typography>
+			</Grid>
+		</HeroLayout>
+	)
 }
 
 export default DonatePage
